0b1126f83d754ee66a6bb1f76db8c5699c7dec2a
Andreas Arvidsson <andreas.arvidsson87@gmail.com>
2021-06-11 14:11:20 +0200

    Added timeout for getStdin and trim trailing new lines

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 'use strict';
 const meow = require('meow');
-const getStdin = require('get-stdin');
+const getStdin = require('stdin2');
 const clipboardy = require('clipboardy');
 
 meow(`
@@ -15,6 +15,13 @@ if (process.stdin.isTTY || process.env.STDIN === '0') {
 	process.stdout.write(clipboardy.readSync());
 } else {
 	(async () => {
-		clipboardy.writeSync(await getStdin());
+        try {
+            const text = await getStdin({ timeout: 30 });
+            clipboardy.writeSync(text.replace(/(\r\n|\n|\r)$/gm, " "));
+        }
+        catch (e) {
+            process.stdout.write(clipboardy.readSync());
+            process.exit(0);
+        }
 	})();
 }
