23a5e47faa0f80a416eb2043f9ed1a7ce8c6c735
Sindre Sorhus <sindresorhus@gmail.com>
2026-02-03 19:05:25 +0700

    Add test for large multiline input
    Reproduces sindresorhus/clipboard-cli#9

--- test.js
+++ test.js
@@ -1,8 +1,24 @@
 import test from 'ava';
 import {execa} from 'execa';
 
-test('main', async t => {
+test.serial('main', async t => {
 	const input = '🦄';
+	await execa('./cli.js', {input});
+
+	const {stdout} = await execa('./cli.js', {
+		env: {
+			STDIN: 0,
+		},
+	});
+
+	t.is(stdout, input);
+});
+
+test.serial('large multiline input', async t => {
+	const line = 'I am a pink elephant. Do not think about me';
+	const lineCount = 5000;
+	const input = Array.from({length: lineCount}, () => line).join('\n');
+
 	await execa('./cli.js', {input});
 
 	const {stdout} = await execa('./cli.js', {
